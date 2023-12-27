8a208e760f28f4ac3b392009a7aa4ba9bf01b013
teddy0@arch-thinkpad <theodore_ong@berkeley.edu>
2023-12-27 09:18:28 +0800

    parsed parameters as string

--- src/lib/simulation/monte_carlo/openmc/open_mc_simulation_environment/openmc_settings/openmc_xml_serde.rs
+++ src/lib/simulation/monte_carlo/openmc/open_mc_simulation_environment/openmc_settings/openmc_xml_serde.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use serde_xml_rs::{from_str, to_string};
 
 use std::{fs, path::PathBuf};
 
@@ -26,18 +25,34 @@ struct Settings {
 struct OpenMCSource {
     particle: String,
     strength: String,
-    r#type: SourceType,
+    r#type: OpenMCSourceType,
+    space: OpenMCSourceSpace,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[allow(non_camel_case_types)]
-enum SourceType {
+enum OpenMCSourceType {
     independent,
 }
 
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[allow(non_camel_case_types)]
+struct OpenMCSourceSpace {
+    r#type: OpenMCSpaceType,
+    parameters: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[allow(non_camel_case_types)]
+enum OpenMCSpaceType {
+    fission,
+}
+
 #[test]
 pub fn simplest_serde_test_openmc_xml(){
+    use serde_xml_rs::{from_str, to_string};
     let test_settings_xml_filepath: PathBuf = 
         "./src/lib/simulation/monte_carlo/openmc/open_mc_simulation_environment/assembly_settings_example.xml".into();
 
@@ -55,6 +70,7 @@ pub fn simplest_serde_test_openmc_xml(){
 
 #[test]
 pub fn tag_serde_test_openmc_xml(){
+    use serde_xml_rs::{from_str, to_string};
     let test_settings_xml_filepath: PathBuf = 
         "./src/lib/simulation/monte_carlo/openmc/open_mc_simulation_environment/assembly_settings_example.xml".into();
 
@@ -66,7 +82,9 @@ pub fn tag_serde_test_openmc_xml(){
     assert_eq!(settings.run_mode, "eigenvalue".to_string());
     assert_eq!(settings.source.particle, "neutron".to_string());
     assert_eq!(settings.source.strength, "1.0".to_string());
-    assert_eq!(settings.source.r#type, SourceType::independent);
+    assert_eq!(settings.source.r#type, OpenMCSourceType::independent);
+    assert_eq!(settings.source.space.r#type, OpenMCSpaceType::fission);
+    assert_eq!(settings.source.space.parameters, "-10.71 -10.71 -1 10.71 10.71 1");
 
 }
 
