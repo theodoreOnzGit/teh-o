ecd9e66097fe02fa21533060e9b6bfbd80782d91
teddy0@arch-thinkpad <theodore_ong@berkeley.edu>
2023-12-28 14:33:28 +0800

    opened u235 file

--- src/lib/simulation/monte_carlo/openmc/openmc_hdf5_interface/mod.rs
+++ src/lib/simulation/monte_carlo/openmc/openmc_hdf5_interface/mod.rs
@@ -1,11 +1,9 @@
-use crate::teh_o_error::TehOError;
-
 
 #[test]
-pub fn read_u235_h5() -> Result<(),TehOError>{
+pub fn read_u235_h5() -> Result<(), crate::teh_o_error::TehOError>{
 
     let file = hdf5::File::open("./src/lib/simulation/monte_carlo/openmc/openmc_nuclides/U235.h5")?;
-    let ds = file;
+    let ds = file.dataset("/U235/energy/294K");
     dbg!(&ds);
     Ok(())
 }
