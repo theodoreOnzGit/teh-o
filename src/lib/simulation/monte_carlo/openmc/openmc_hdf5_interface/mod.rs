0ba7c0c469eb143e02f625b0a1d6180edb2318c6
teddy0@arch-thinkpad <theodore_ong@berkeley.edu>
2023-12-28 15:19:13 +0800

    able to list subgroups now

--- src/lib/simulation/monte_carlo/openmc/openmc_hdf5_interface/mod.rs
+++ src/lib/simulation/monte_carlo/openmc/openmc_hdf5_interface/mod.rs
@@ -20,3 +20,31 @@ pub fn read_u235_data() -> Result<(), crate::teh_o_error::TehOError>{
     assert_eq!(*energy_level,1.03125e-5);
     Ok(())
 }
+
+#[test]
+pub fn read_u235_folder() -> Result<(), crate::teh_o_error::TehOError>{
+
+    let file = hdf5::File::open("./src/lib/simulation/monte_carlo/openmc/openmc_nuclides/U235.h5")?;
+    // let's try to get a list of folders available
+    
+    let names_of_groups = file.member_names()?;
+    assert_eq!(names_of_groups[0],"U235");
+
+    Ok(())
+}
+#[test]
+pub fn read_u235_subgroups() -> Result<(), crate::teh_o_error::TehOError>{
+
+    let file = hdf5::File::open("./src/lib/simulation/monte_carlo/openmc/openmc_nuclides/U235.h5")?;
+    // let's try to get a list of folders available
+    
+    let main_group: hdf5::Group = file.as_group()?;
+    let u235_group: Vec<hdf5::Group> = main_group.groups()?;
+    let subgroups = u235_group[0].groups()?;
+
+    dbg!(&u235_group);
+    dbg!(&subgroups);
+
+
+    Ok(())
+}
