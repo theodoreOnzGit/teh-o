6294b739e09f3489f4e5a7fd5752d1021b825a4b
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-02-07 10:11:05 +0800

    created new binary for mock test cases

---
+++ src/mock_test_cases.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/// contains code for simplified monte carlo cases
+/// that don't require classes and such
+/// just for the understanding of Monte Carlo
+pub mod mock_test_cases_lib;
