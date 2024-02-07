4c5b083357eeaccab89e8bed7457110af16da0c3
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-02-07 14:12:06 +0800

    added absorption random walk and scattering random walk

--- src/mock_test_cases_lib/spherical_u235/monoenergetic/particles_and_surfaces/mod.rs
+++ src/mock_test_cases_lib/spherical_u235/monoenergetic/particles_and_surfaces/mod.rs
@@ -1,2 +1,10 @@
+/// Example 1:
+/// The first test to understand and develop Monte Carlo code
+/// This is a single particle doing a random walk in an infinite medium
+/// and getting absorbed at the first interaction
 pub mod absorption_random_walk;
 
+/// Example 2: 
+/// This is a single particle doing a random walk in an infinite medium
+/// and getting scattered a few times until it gets absorbed
+pub mod scattering_random_walk;
