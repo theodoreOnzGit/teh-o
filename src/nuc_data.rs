3db37460a463e7a71d02c7eca444ae5e28061992
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-02-29 08:53:56 +0800

    added a binary file to read u235 hdf5 file formats in openmc in debug mode, seems to work

--- src/nuc_data.rs
+++ src/nuc_data.rs
@@ -33,6 +33,51 @@
 // and change them into toml files suitable for Rust, then I want to 
 // make a new crate which is independent of hdf5 files and only uses toml 
 // files.
+//
+//
 fn main() {
     println!("Hello, world!");
+
+    read_u235_data_to_toml().unwrap();
+
 }
+
+
+// in this test, i want to read U235 (n,f) cross sections and plot them 
+// out into a toml file
+pub fn read_u235_data_to_toml() -> Result<(), teh_o::teh_o_error::TehOError>{
+
+    let file = hdf5::File::open("./src/lib/simulation/monte_carlo/openmc/openmc_nuclides/U235.h5")?;
+    let ds_energy_levels = file.dataset("/U235/energy/294K")?;
+    let u235_energy_array = ds_energy_levels.read_1d::<f64>()?;
+
+    //// this shows energy in eV
+    //dbg!(&u235_energy_array);
+
+
+    let u235_group: Vec<hdf5::Group> = file.as_group()?.groups()?;
+    let subgroups = u235_group.first().unwrap().groups()?;
+
+    for group in subgroups.iter() {
+        //dbg!(&group.name());
+        //dbg!(group.member_names()?);
+    }
+
+    // hdf5 group for fission cross section
+    let group_cross_sections_n_fission = file.group("/U235/reactions/reaction_018/294K")?;
+
+    //dbg!(&group_cross_sections_n_fission);
+
+    let n_f_member_names = group_cross_sections_n_fission.member_names()?;
+
+    //dbg!(&n_f_member_names);
+
+    let fission_dataset = group_cross_sections_n_fission.dataset("xs")?;
+    //dbg!(&fission_dataset);
+    let u235_fission_array_294K = fission_dataset.read_1d::<f64>()?;
+
+    //dbg!(&u235_fission_array_294K);
+
+    Ok(())
+}
+
